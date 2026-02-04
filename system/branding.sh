6244e6c2c53b142f0dbb9a30514e94c40da829c3
m-fe02 <matthewrust21@gmail.com>
2026-02-04 08:42:53 -0600

    Add custom branding

---
+++ system/branding.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+echo "Applying Hackpad OS Branding..."
+
+# 1. Generate /etc/os-release
+cat <<EOF > /etc/os-release
+NAME="Hackpad OS"
+VERSION="43"
+ID=hackpad
+ID_LIKE=fedora
+VERSION_ID=43
+PRETTY_NAME="Hackpad OS 43 (Atomic)"
+ANSI_COLOR="0;34"
+CPE_NAME="cpe:/o:hackpad:hackpad:43"
+HOME_URL="https://github.com/m-fe02/fedora-bootc"
+VARIANT="Developer"
+BUILD_ID=$(date +%Y%m%d)
+EOF
+
+# Sync to /usr/lib for system tool compatibility
+ln -sf /etc/os-release /usr/lib/os-release
+
+# 2. Generate /etc/issue and /etc/issue.net
+cat <<'EOF' > /etc/issue
+                  _                     _
+  /\  /\__ _  ___| | ___ __   __ _  __| |
+ / /_/ / _` |/ __| |/ / '_ \ / _` |/ _` |
+/ __  / (_| | (__|   <| |_) | (_| | (_| |
+\/ /_/ \__,_|\___|_|\_\ .__/ \__,_|\__,_|
+                      |_|
+
+This is YOUR Hackpad (\l)
+Kernel \r
+EOF
+
+# Use the same file for remote logins
+cp /etc/issue /etc/issue.net
+
+echo "Identity branding complete."
