a1a7476007d06df50a79932b95c64a29ad7abd41
m-fe02 <matthewrust21@gmail.com>
2026-03-18 23:18:17 -0500

    Remove redundant /usr/lib/modules cleanup - dnf remove already handles it

--- files/scripts/install-cachyos-kernel.sh
+++ files/scripts/install-cachyos-kernel.sh
@@ -3,7 +3,6 @@ set -ex
 
 # Install CachyOS Kernel
 dnf -y remove kernel kernel-*
-rm -rf /usr/lib/modules/*
 dnf -y copr enable bieszczaders/kernel-cachyos-lto
 dnf -y install --setopt=install_weak_deps=False kernel-cachyos-lto
 dnf -y copr disable bieszczaders/kernel-cachyos-lto
