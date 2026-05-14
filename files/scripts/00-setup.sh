c0a9a1aaa97f37538ece75928bcde003ee62bf96
m-fe02 <matthewrust21@gmail.com>
2026-05-14 14:21:11 -0500

    Forgot to invoke the new script

--- files/scripts/00-setup.sh
+++ files/scripts/00-setup.sh
@@ -14,8 +14,11 @@ bash "$BUILD_SETUP_DIR/01-upgrade-packages.sh"
 echo "Step 3: Installing packages..."
 bash "$BUILD_SETUP_DIR/05-install-pkgs.sh"
 
-echo "Step 4: Regenerating initramfs..."
+echo "Step 4: Removing unwanted desktop entries..."
+bash "$BUILD_SETUP_DIR/06-remove-desktop-entries.sh"
+
+echo "Step 5: Regenerating initramfs..."
 bash "$BUILD_SETUP_DIR/57-initramfs.sh"
 
-echo "Step 5: Running cleanup..."
+echo "Step 6: Running cleanup..."
 bash "$BUILD_SETUP_DIR/58-post-setup.sh"
